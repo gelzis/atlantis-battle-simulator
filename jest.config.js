864065f9a00cf2c790dfc31a9b50fc0f249498ef
gelzis <gelzis@gelzis.com>
2026-06-13 21:28:36 +0300

    adjust jest config for uuid module

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    transform: {
+        '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+        '/node_modules/(?!(uuid)/)',
+    ],
 };
