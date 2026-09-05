42de3c92888db63c48018cfbec02a8f058e41422
gelzis <gelzis@gelzis.com>
2026-09-05 16:02:49 +0300

    Update Atlantis Discord contact link

--- src/frontend/BattleSimulator/components/PageFooter.tsx
+++ src/frontend/BattleSimulator/components/PageFooter.tsx
@@ -13,8 +13,9 @@ const Footer = styled(Typography)`
 export const PageFooter: FC = () => {
     return (
         <Footer variant="body2">
-            Copyright © Raivis Gelsbergs {`${new Date().getFullYear()}`}. <br/>You can report bugs in <a target="_blank" href="https://github.com/gelzis/atlantis-battle-simulator">Github</a> and/or contact me on <a href="https://discord.gg/HusGETf">discord</a>, my name tag is Gelzis#9633. <br/>
-            <a style={{color: '#000000'}} target="_blank" href="https://github.com/gelzis/atlantis-battle-simulator">
+            Copyright © Raivis Gelsbergs {`${new Date().getFullYear()}`}. <br/>
+            Report bugs on <a target="_blank" rel="noopener noreferrer" href="https://github.com/gelzis/atlantis-battle-simulator">GitHub</a> or contact me in the <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/wSvPT9x8NT">Atlantis PBEM Discord</a>. <br/>
+            <a style={{color: '#000000'}} target="_blank" rel="noopener noreferrer" href="https://github.com/gelzis/atlantis-battle-simulator">
                 <GitHubIcon color="inherit"/>
             </a>
         </Footer>
